f2d23566e0efb4fe5e7e462af48b82ae29c01340
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-13 17:43:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,17 @@ const stepsMin = 0
 const stepsMax = 5000
 
 class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      water :0,
+      heart : 120,
+      temperature : -10,
+      steps : 3000
+    }
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -20,16 +31,34 @@ class App extends React.Component {
           <p>steps : {stepsMin}</p>
 
           {/* Water */}
-          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L"/>
+          <Box 
+            icon="local_drink" 
+            color="#3A85FF" 
+            value={1.5} 
+            unit="L"
+          />
 
           {/* Steps */}
-          <Box icon="directions_walk" color="black" value={3000} unit="steps"/>
+          <Box 
+            icon="directions_walk" 
+            color="black" 
+            value={3000} 
+            unit="steps"
+          />
 
           {/* Heart */}
-          <Box icon="favorite" color="red" value={120} unit="bpm"/>
+          <Box icon="favorite" 
+            color="red" 
+            value={120} 
+            unit="bpm"
+          />
 
           {/* Temperature */}
-          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C"/>
+          <Box icon="wb_sunny" 
+            color="yellow" 
+            value={-10} 
+            unit="°C"
+          />
           
         </div>
       </div>
--- src/components/Box.js
+++ src/components/Box.js
@@ -2,12 +2,14 @@ import React from "react";
 
 class Box extends React.Component{
     render() {
+        const isWater = this.props.icon !== "local_drink"
         return(
         <div className="box col-sm-3 col-6">
             <span className= "material-icons" style={{color: this.props.color, fontSize: '100px'}}>
                 {this.props.icon}
             </span>
             <p> {this.props.value} {this.props.unit}</p>
+            { isWater && <input type="range" id="" name="" min="0" max="11"></input>} 
         </div>
     );
 }
